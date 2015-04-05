aaae7e5b268de0a45b0adbb5ab2a21130d20a6cf
at4260 <at4260@gmail.com>
2015-04-05 12:51:08 -0700

    Added unit tests.

---
+++ unit_tests.py
@@ -0,0 +1,43 @@
+import unittest
+import utils
+
+
+class FibonacciTestCase(unittest.TestCase):
+    def test_user_results_zero(self):
+        self.assertEqual(utils.get_fibonacci(
+            0), 0)
+
+    def test_user_results_one(self):
+        self.assertEqual(utils.get_fibonacci(
+            1), 1)
+
+    def test_user_results_two(self):
+        self.assertEqual(utils.get_fibonacci(
+            2), 1)
+
+    def test_user_results_five(self):
+        self.assertEqual(utils.get_fibonacci(
+            5), 5)
+
+    def test_user_results_seven(self):
+        self.assertEqual(utils.get_fibonacci(
+            7), 13)
+
+    def test_user_results_ten(self):
+        self.assertEqual(utils.get_fibonacci(
+            10), 55)
+
+    def test_user_results_string(self):
+        self.assertEqual(utils.get_fibonacci(
+            "test"), None)
+
+    def test_user_results_neg(self):
+        self.assertEqual(utils.get_fibonacci(
+            -1), None)
+
+    def test_user_results_float(self):
+        self.assertEqual(utils.get_fibonacci(
+            4.5), None)
+
+if __name__ == "__main__":
+    unittest.main()
