73286e1deabeb56de937f0e5e35dcce175165b78
at4260 <at4260@gmail.com>
2015-04-05 14:06:19 -0700

    Refactored fibonacci calculation.

--- utils.py
+++ utils.py
@@ -1,15 +1,10 @@
 def get_fibonacci(n):
-    fib_list = []
-    index = 0
     if type(n) is not int or n < 0:
         return None
     else:
+        fib_list = [0,1]
+        index = 2
         while index <= n:
-            if index == 0:
-                fib_list.append(0)
-            elif index == 1:
-                fib_list.append(1)
-            else:
-                fib_list.append(fib_list[index - 1] + fib_list[index - 2])
+            fib_list.append(fib_list[index - 1] + fib_list[index - 2])
             index += 1
         return fib_list[n]
