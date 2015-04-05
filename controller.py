9841d6392cecdb43478b7f7e39020411a2933748
at4260 <at4260@gmail.com>
2015-04-05 13:48:02 -0700

    Replaced Gunicorn with Gevent to load server on port 8000.

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import gevent
 import utils
 
 from flask import Flask, render_template
+from gevent import wsgi
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -13,6 +14,5 @@ def get_fib_value(n):
     print fib_value
     return render_template("fib.html", fib_value = fib_value)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+server = wsgi.WSGIServer(('127.0.0.1', 8000),app)
+server.serve_forever()
