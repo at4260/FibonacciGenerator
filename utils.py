720884ba21e7962ca98a95b98a76ef9e5e29663a
at4260 <at4260@gmail.com>
2015-04-05 22:17:14 -0700

    Refactored fibonacci function for better efficiency.

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 def get_fibonacci(n):
+    value_0 = 0
+    value_1 = 1
     if n < 0:
         return "Not valid"
     elif n == 0:
-        return 0
+        return value_0
     elif n == 1:
-        return 1
+        return value_1
     else:
-        return get_fibonacci(n - 1) + get_fibonacci(n - 2)
+        while n >= 2:
+            sum_of_values = value_0 + value_1
+            value_0 = value_1
+            value_1 = sum_of_values
+            n = n - 1
+        return sum_of_values
