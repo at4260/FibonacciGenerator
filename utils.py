d28acd0e0c617162d4867ff345fca158ea4197ba
at4260 <at4260@gmail.com>
2015-04-07 13:15:54 -0700

    Created decorator to cache fib results.

--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
+def memoize(func):
+    cached = {}
+    def helper(x):
+        if x not in cached:
+            cached[x] = func(x)
+            # print cached
+        return cached[x]
+    return helper
+
+@memoize
 def get_fibonacci(n):
-    first_value = 0
-    second_value = 1
     if n < 0:
         return "Not valid"
     elif n == 0:
-        return first_value
+        return 0
     elif n == 1:
-        return second_value
+        return 1
     else:
-        while n >= 2:
-            sum_of_values = first_value + second_value
-            first_value = second_value
-            second_value = sum_of_values
-            n = n - 1
-        return sum_of_values
+        return get_fibonacci(n - 1) + get_fibonacci(n - 2)
